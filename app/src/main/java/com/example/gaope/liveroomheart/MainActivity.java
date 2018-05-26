1c84e907bfeb8033278751f58a9b667ce9f4a631
gaopei <gaopeigpgp@126.com>
2018-05-26 22:23:33 +0800

    PathMeasure之直播间送爱心：Matrix增加了Heart的缩放和图片的透明度

--- app/src/main/java/com/example/gaope/liveroomheart/MainActivity.java
+++ app/src/main/java/com/example/gaope/liveroomheart/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         liveHeartView = (LiveHeartView) findViewById(R.id.live_view);
+        //使用liveHeartView.setOnClickListener这种方法时，LiveHeartView的View对象已经创建好啦，
+        //LiveHeartView的构造方法，onSizeChanged方法依次执行，再调用liveHeartView的addHeart()方法
         liveHeartView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
 //        for (int i = 0;i < 5;i++){
 //            liveHeartView.addHeart();
 //        }
+
+          //使用这种方法时，先调用liveHeartView的addHeart()方法，再创建LiveHeartView的View对象，
+          //然后LiveHeartView的构造方法，onSizeChanged方法依次执行，
 //        liveHeartView.addHeart();
 
     }
